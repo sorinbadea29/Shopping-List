222392e63fa4f2d2851b874df67e84d2d3542c04
Sorin Badea <62335902+sorinbadea29@users.noreply.github.com>
2020-04-25 17:35:48 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -12,98 +12,100 @@ document.addEventListener('DOMContentLoaded', () => {
     filter.addEventListener('keyup', ui.filterItems);
 });
 
-function Item(addInput, quantInput, unitInput){
-    this.addInput = addInput;
-    this.quantInput = quantInput;
-    this.unitInput = unitInput;
+class Item{
+    constructor(addInput, quantInput, unitInput){
+        this.addInput = addInput;
+        this.quantInput = quantInput;
+        this.unitInput = unitInput;
+    };
 };
 let item = new Item;
 
-function Ui(){};
+class Ui{
+    createItem(item){
+        let row = document.createElement('div');
+        row.classList.add('row');
+        let rowContent = `
+        <button class="btn check"><i class="fas fa-check"></i></button>
+        <p class="item list-item">${item.addInput.value}</p>
+        <p class="quant-select">${item.quantInput.value}</p>
+        <p class="unit-select">${item.unitInput.value}</p>
+        <input class="price" type="number" min="0">
+        <button class="btn btn-danger"><i class="fas fa-trash "></i></button>`;
+        row.innerHTML = rowContent;
+        cartItems.append(row);
+    
+        if(cartItems !== ''){
+            labels.classList.add('show-flex');
+        }else{
+            labels.classList.remove('show-flex');
+        };
+    };
+    
+    setMessage(nameOfClass, message, time){
+        let msg = document.querySelector('.msg');
+        msg.classList.add(nameOfClass);
+        msg.innerText = message;
+        setTimeout(() => msg.classList.remove(nameOfClass), time);   
+    };
+    
+    checkItem(target){
+        target.parentElement.parentElement.classList.toggle('toggleCheck');
+    };
+    
+    deletItem(target){
+        target.parentElement.parentElement.remove();
+        if(cartItems.textContent === ''){
+            labels.classList.remove('show-flex');
+        };
+    };
+    
+    updateCartTotal(total){
+        total = 0;
+        let rows = document.querySelectorAll('.row');
+        rows.forEach(function(row){
+            let priceElement = row.querySelector('.price');
+            let price = Number(priceElement.value);
+            price === 0;
+            if(isNaN(price) || price <= 0){
+                price = 0;
+                priceElement.classList.add('red');
+            }else{
+                price = price;
+                priceElement.classList.add('green');
+            };
+            total += price;
+        });
+        total = Math.round(total * 100) / 100; 
+        document.getElementById('total-value').innerText = `${total} eur`;
+    
+        if(total != 0){
+            totalElement.classList.add('show');
+        }else{
+            totalElement.classList.remove('show');
+        };
+    };
+    
+    clearInputs(){
+        addInput.value = '';
+        quantInput.value = '';
+        unitInput.value = '';
+    };
+    
+    filterItems(e){
+        let filterText = e.target.value.toLowerCase();
+        let items = document.getElementsByClassName('item');
+        Array.from(items).forEach(function(item){
+            let itemText = item.textContent;
+            if(itemText.indexOf(filterText) != -1){
+                item.parentElement.style.display = 'flex';
+            }else{
+                item.parentElement.style.display = 'none';
+            };
+        });
+    };
+};
 let ui = new Ui;
-
-Ui.prototype.createItem = function(item){
-    let row = document.createElement('div');
-    row.classList.add('row');
-    let rowContent = `
-    <button class="btn check"><i class="fas fa-check"></i></button>
-    <p class="item list-item">${item.addInput.value}</p>
-    <p class="quant-select">${item.quantInput.value}</p>
-    <p class="unit-select">${item.unitInput.value}</p>
-    <input class="price" type="number" min="0">
-    <button class="btn btn-danger"><i class="fas fa-trash "></i></button>`;
-    row.innerHTML = rowContent;
-    cartItems.append(row);
-
-    if(cartItems !== ''){
-        labels.classList.add('show-flex');
-    }else{
-        labels.classList.remove('show-flex');
-    };
-};
-
-Ui.prototype.setMessage = function(nameOfClass, message, time){
-    let msg = document.querySelector('.msg');
-    msg.classList.add(nameOfClass);
-    msg.innerText = message;
-    setTimeout(() => msg.classList.remove(nameOfClass), time);   
-};
-
-Ui.prototype.checkItem = function(target){
-    target.parentElement.parentElement.classList.toggle('toggleCheck');
-};
-
-Ui.prototype.deletItem = function(target){
-    target.parentElement.parentElement.remove();
-    if(cartItems.textContent === ''){
-        labels.classList.remove('show-flex');
-    };
-};
-
-Ui.prototype.updateCartTotal = function(total){
-    total = 0;
-    let rows = document.querySelectorAll('.row');
-    rows.forEach(function(row){
-        let priceElement = row.querySelector('.price');
-        let price = Number(priceElement.value);
-        price === 0;
-        if(isNaN(price) || price <= 0){
-            price = 0;
-            priceElement.classList.add('red');
-        }else{
-            price = price;
-            priceElement.classList.add('green');
-        };
-        total += price;
-    });
-    total = Math.round(total * 100) / 100; 
-    document.getElementById('total-value').innerText = `${total} eur`;
-
-    if(total != 0){
-        totalElement.classList.add('show');
-    }else{
-        totalElement.classList.remove('show');
-    };
-};
-
-Ui.prototype.clearInputs = function(){
-    addInput.value = '';
-    quantInput.value = '';
-    unitInput.value = '';
-};
-
-Ui.prototype.filterItems = function(e){
-    let filterText = e.target.value.toLowerCase();
-    let items = document.getElementsByClassName('item');
-    Array.from(items).forEach(function(item){
-        let itemText = item.textContent;
-        if(itemText.indexOf(filterText) != -1){
-            item.parentElement.style.display = 'flex';
-        }else{
-            item.parentElement.style.display = 'none';
-        };
-    });
-};
 
 
 function addToList(e){
